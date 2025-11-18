d35840f67e7419e7e75557c4e5d54499b1840eda
AsiA203 <aselia.azimkanova@gmail.com>
2025-11-17 22:21:40 -0600

    1. Created AuthControllerTest.java

---
+++ src/test/java/com/example/usefy/AuthControllerTest.java
@@ -0,0 +1,10 @@
+package com.example.usefy;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthControllerTest {
+
+}
