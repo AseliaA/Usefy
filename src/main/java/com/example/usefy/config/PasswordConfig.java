fc043aa9d01abc604204d98fc398789443fba9fb
AsiA203 <aselia.azimkanova@gmail.com>
2025-11-24 22:18:05 -0600

    Added default username and password for application

--- src/main/java/com/example/usefy/config/PasswordConfig.java
+++ src/main/java/com/example/usefy/config/PasswordConfig.java
@@ -2,10 +2,8 @@ package com.example.usefy.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class PasswordConfig {
@@ -13,20 +11,5 @@ public class PasswordConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .headers().frameOptions().disable()
-                .and()
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("auth/**", "/home", "/h2-console").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .formLogin().disable()
-                .httpBasic().disable();
-        return http.build();
     }
 }
